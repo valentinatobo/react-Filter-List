8f38998e529062c8f67625b7778f5f981f131875
valentinatobo <36264543+valentinatobo@users.noreply.github.com>
2022-09-30 16:51:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-Filter-List
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yt4rtm)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import List from './list/List';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello desde React!</h1>
+      <List />
+    </div>
+  );
+}
